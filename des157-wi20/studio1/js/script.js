d291cf7ccfea1c71aa91a110a299d779c885e4fb
Megan Frentzel <mkfrentzel@ucdavis.edu>
2020-02-11 00:37:14 -0800

    added academic file and updated madlib

--- des157-wi20/studio1/js/script.js
+++ des157-wi20/studio1/js/script.js
@@ -1,18 +1,65 @@
-function setup() {
-    var myCanvas=createCanvas(800, 250);
-    frameRate(8);
-    noStroke();
-    background(random(255), random(255), random(255));
+(function () {
 
-    myCanvas.parent(mySketch);
-  }
+  "use strict"  
+
+  let fadeIn = $( "#fadeInButton" );
+  $("#fadeInButton").fadeIn();
   
-  function draw() {
-    if(mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(random(255), random(255), random(255), random(255));
+    
+
+// listen for the submission of the form
+document.getElementById("myform").addEventListener("submit", function(event){
+    // prevent the default behavior
+    event.preventDefault();
+
+    // get all the text fields from the form
+    const formData = document.querySelectorAll("input[type=text]");
+    // create an array to hold values from the form
+    const words = [];
+
+    //loop through the fields and add the words to the array, one at a time.
+    for( let i=0; i<formData.length; i++){
+        words.push(formData[i].value);    
     }
-      var circSize = random(200);
-    ellipse(mouseX, mouseY, circSize, circSize);
-  }
+    //You can see the array in the console, if you want...
+    console.log(words);
+
+    //run a function to put the words into the madlib... Function below...
+    //Pass the array of words the user typed into the function
+    makeMadLib(words);
+
+});
+
+//This function takes an array input, then constructs the madlib, peppering
+//in the user's words where necessary.
+function makeMadLib(theWords){
+    const madlib = `I went to Woodstock's and ordered a pizza with ${theWords[0]}. When the pizza got to my table it smelled ${theWords[1]}. It was so ${theWords[2]} I was in complete shock. My good pal ${theWords[3]} tried to steal a slice and I ${theWords[4]} it out of their mouth!`;
+
+    //Get the madlib container
+    const mlContainer = document.getElementById('madlib');
+
+    //Change the guts of the container to your madlib
+    mlContainer.innerHTML = `<p>${madlib}</p>`;
+
+    //change the class on the madlib container so you can see it.
+    mlContainer.setAttribute("class", "visible");
+
+
+
+}
+
+/*******************************
+TO DO:
+1. Add interesting graphics/fun madlib content/theme
+2. Change styling (add fonts from google fonts, colors, etc)
+3. Change the way the madlib appears (add a css animation?)
+4. Adjust JS to suit your madlib
+5. Refactor JS (put in external script file, put in enclosure, 
+change var to const or let, etc.)
+6. Have fun!
+*********************************/
+
+
+
+    
+} ());
